51e78b7c67fab9e95ac4fe33ddfd47ddbfaa8488
Farhatul Hassan <farhatulhassan1@gmail.com>
2026-01-13 16:47:06 +0600

    installed socket io to server.js

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,37 @@ import mongoose from "mongoose";
 import authRouter from "./routes/auth.js";
 import postRouter from "./routes/post.js";
 import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
 import commentRouter from "./routes/comment.js";
 const app = express();
 const PORT = 4000;
+const server = http.createServer(app);
+//socket.io
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
 
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    socket.on("join_room", (roomId) => {
+        console.log(`User ${socket.id} joined the room ${room}`);
+    });
+    socket.on("send_message", (data) => {
+        socket.to(data.roomId).emit("receive_message", data);
+    });
+    socket.on("typing", ({ username, roomId }) => {
+        socket.to(room).emit("user_typing", username);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+    });
+});
 //database connection
 mongoose.connect(process.env.MONGODB_URL);
 const db = mongoose.connection;
@@ -32,6 +59,6 @@ app.use("/auth", authRouter);
 app.use("/posts", postRouter);
 app.use("/posts", commentRouter);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("Server is running at 4000 port");
 });
