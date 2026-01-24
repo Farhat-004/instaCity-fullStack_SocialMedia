e4dcfb9165a4c0aac0a43030b10a640067cdd217
Farhatul Hassan <farhatulhassan1@gmail.com>
2026-01-24 18:16:03 +0600

    change homePage style

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import PostsList from "../components/posts/PostList";
 
 export default function HomePage() {
     return (
-        <div className="max-w-6xl mx-auto w-full py-10  ">
+        <div className="max-w-6xl mx-auto w-full md:py-10  ">
             <div style={{ height: "100vh" }}>
                 <PostsList />
             </div>
